098208cb139ac691c8c1dc8d1e7b4e800a92b0cc
aragornAGH <jkolodzi@student.agh.edu.pl>
2014-11-17 00:46:56 +0100

    Poprawki algorytmy, przykładowy set

--- src/pl/edu/agh/io/bayes/BayesClassificator.java
+++ src/pl/edu/agh/io/bayes/BayesClassificator.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +19,9 @@ import com.google.common.io.Files;
  */
 public class BayesClassificator {
 
+	public static Map<String, HashMap<String, HashMap<String, Double>>> probabilityMap = new HashMap<String, HashMap<String, HashMap<String, Double>>>();
+	public static Map<String, Long> classes = new HashMap<String, Long>();
+
 	/**
 	 * Metoda wczytujaca dane z pliku
 	 * 
@@ -31,15 +35,23 @@ public class BayesClassificator {
 	 *             problem z odczytaniem pliku
 	 */
 	public static List<ObjectRow> readTeachingDataFromFile(File file,
-			int classColumn) throws IOException {
+			int classColumn, String separator) throws IOException {
 		List<ObjectRow> objects = new ArrayList<ObjectRow>();
 		List<String> rawData = Files.readLines(file, Charsets.UTF_8);
+		int u = 1;
 		for (String s : rawData) {
+
+			System.out.println("TEACHING : " + u + " / " + rawData.size());
 			ObjectRow raw = new ObjectRow();
 			int i = 1;
+			if (s.startsWith("  ")) {
+				s = s.substring(2);
+			}
 			String afterTrim = s.trim().replaceAll(" +", " ");
-			afterTrim = afterTrim.replaceAll("	", " ");
-			for (String prop : afterTrim.split(" ")) {
+			afterTrim = afterTrim.replaceAll("\t", "");
+			afterTrim = afterTrim.replaceAll("\n", "");
+			afterTrim = afterTrim.replaceAll("	", "");
+			for (String prop : afterTrim.split(separator)) {
 				if (i == classColumn) {
 					raw.setClassName(prop);
 				} else {
@@ -48,6 +60,46 @@ public class BayesClassificator {
 				i++;
 			}
 			objects.add(raw);
+
+			if (classes.get(raw.getClassName()) == null) {
+				classes.put(raw.getClassName(), 1L);
+			} else {
+				Long previous = classes.get(raw.getClassName());
+				classes.remove(raw.getClassName());
+				classes.put(raw.getClassName(), previous + 1L);
+			}
+
+			if (probabilityMap.get(raw.getClassName()) == null) {
+				HashMap<String, HashMap<String, Double>> pMap = new HashMap<String, HashMap<String, Double>>();
+				probabilityMap.put(raw.getClassName(), pMap);
+			}
+			for (String paramName : raw.getParameters().keySet()) {
+				if (probabilityMap.get(raw.getClassName()).get(paramName) == null) {
+					HashMap<String, Double> paramMap = new HashMap<String, Double>();
+					paramMap.put(raw.getParameters().get(paramName), 1.0);
+					probabilityMap.get(raw.getClassName()).put(paramName,
+							paramMap);
+				} else {
+					if (probabilityMap.get(raw.getClassName()).get(paramName)
+							.get(raw.getParameters().get(paramName)) == null) {
+						probabilityMap.get(raw.getClassName()).get(paramName)
+								.put(raw.getParameters().get(paramName), 1.0);
+					} else {
+						Double previous = ((Double) probabilityMap
+								.get(raw.getClassName()).get(paramName)
+								.get(raw.getParameters().get(paramName)));
+						probabilityMap.get(raw.getClassName()).get(paramName)
+								.remove(raw.getParameters().get(paramName));
+						probabilityMap
+								.get(raw.getClassName())
+								.get(paramName)
+								.put(raw.getParameters().get(paramName),
+										previous + 1.0);
+					}
+				}
+
+			}
+			u++;
 		}
 		System.out.println("Data readed");
 		return objects;
@@ -70,23 +122,31 @@ public class BayesClassificator {
 	 */
 	private static double countProbabilityParameterInClass(String className,
 			String parameter, String parameterValue, List<ObjectRow> objects) {
-		int inClass = 0;
-		int withParameterInClass = 0;
-		for (ObjectRow raw : objects) {
-			if (raw.getClassName() != null
-					&& raw.getClassName().equals(className)) {
-				inClass++;
-				Map<String, String> map = raw.getParameters();
-				for (String param : map.keySet()) {
-					String value = map.get(param);
-					if (param.equals(parameter) && value.equals(parameterValue)) {
-						withParameterInClass++;
-					}
-				}
-			}
-		}
+		// int inClass = 0;
+		// int withParameterInClass = 0;
+		// for (ObjectRow raw : objects) {
+		// if (raw.getClassName() != null
+		// && raw.getClassName().equals(className)) {
+		// inClass++;
+		// Map<String, String> map = raw.getParameters();
+		// for (String param : map.keySet()) {
+		// String value = map.get(param);
+		// if (param.equals(parameter) && value.equals(parameterValue)) {
+		// withParameterInClass++;
+		// }
+		// }
+		// }
+		// }
+		if (probabilityMap.get(className).get(parameter).get(parameterValue) == null) {
+			return 0;
+		} else if (classes.get(className) == null)
+			System.out.println("");
 
-		return ((double) withParameterInClass / inClass) == 1.0 ? 1 :  ((double) withParameterInClass / inClass) * 100;
+		return probabilityMap.get(className).get(parameter).get(parameterValue)
+				/ classes.get(className) * 1000;
+
+		// return ((double) withParameterInClass / inClass) == 1.0 ? 1 :
+		// ((double) withParameterInClass / inClass) * 100;
 	}
 
 	/**
@@ -108,7 +168,9 @@ public class BayesClassificator {
 				classNames.add(raw.getClassName());
 			}
 		}
+
 		List<String> bestClasses = new ArrayList<String>();
+		int i = 1;
 		for (ToClassify tc : tcs) {
 			String bestClass = null;
 			Double percent = 0.0;
@@ -125,6 +187,9 @@ public class BayesClassificator {
 					percent = probability;
 				}
 			}
+			System.out.println("Obiekt zaklasyfikowany: " + bestClass + "   ("
+					+ i + "/" + tcs.size() + ")");
+			i++;
 			bestClasses.add(bestClass);
 		}
 		return bestClasses;
@@ -154,15 +219,22 @@ public class BayesClassificator {
 	 *             problem z plikiem
 	 */
 	private static List<ToClassify> readClassifyDataFromFile(File file,
-			int numOfAttributes) throws IOException {
+			int numOfAttributes, String separator) throws IOException {
 		List<ToClassify> objects = new ArrayList<ToClassify>();
 		List<String> rawData = Files.readLines(file, Charsets.UTF_8);
+		int y = 1;
 		for (String s : rawData) {
+			System.out.println("CLASSIFY : " + y + " / " + rawData.size());
 			ToClassify raw = new ToClassify();
 			int i = 1;
+			if (s.startsWith("  ")) {
+				s = s.substring(2);
+			}
 			String afterTrim = s.trim().replaceAll(" +", " ");
-			afterTrim = afterTrim.replaceAll("	", " ");
-			for (String prop : afterTrim.split(" ")) {
+			afterTrim = afterTrim.replaceAll("\t", "");
+			afterTrim = afterTrim.replaceAll("\n", "");
+			afterTrim = afterTrim.replaceAll("	", "");
+			for (String prop : afterTrim.split(separator)) {
 				if (i == (numOfAttributes + 1)) {
 					// ignore
 				} else {
@@ -171,6 +243,7 @@ public class BayesClassificator {
 				i++;
 			}
 			objects.add(raw);
+			y++;
 		}
 		return objects;
 	}
@@ -218,24 +291,49 @@ public class BayesClassificator {
 		return (double) (Math.round(100 * ((double) sum / (double) size) * 100)) / 100;
 	}
 
+	void makeFile() throws IOException {
+		List<String> lines = Files.readLines(new File("test_test.txt"),
+				Charsets.UTF_8);
+
+		FileWriter fw = new FileWriter(new File("test_ok.txt"));
+
+		for (String s : lines) {
+			int i = 0;
+			String classa = null;
+			for (String splitted : s.split(",")) {
+				if (i == 0) {
+					classa = splitted;
+				} else {
+					fw.write(splitted + ";");
+				}
+				i++;
+			}
+			fw.write(classa + "\n");
+		}
+
+		fw.close();
+	}
+
 	public static void main(String[] args) throws IOException {
-		int numOfAttributes = 561;
-		String teachingData = "human_train.txt";
-		String classifyData = "hs.txt";
+		int numOfAttributes = 35;
+		String teachingData = "train_ok.txt";
+		String classifyData = "test_ok.txt";
 		long start = System.currentTimeMillis();
 		List<String> results = classify(
 				readClassifyDataFromFile(new File(classifyData),
-						numOfAttributes),
+						numOfAttributes, ","),
 				readTeachingDataFromFile(new File(teachingData),
-						numOfAttributes + 1));
-		System.out.println("Pomiar czasu: " + (System.currentTimeMillis()-start)/1000F + " sekund.");
+						numOfAttributes + 1, ","));
+		float time = (System.currentTimeMillis() - start) / 1000F;
+
 		writeOutputOnConsole(results);
 		writeOutput("output.txt", results);
 		System.err.println(compareResults(
 				results,
 				readTeachingDataFromFile(new File(classifyData),
-						numOfAttributes + 1))
-				+ "% wynikow prawidlowo zakwalifikowano");
+						numOfAttributes + 1, ","))
+				+ "% wynikow prawidlowo zakwalifikowano\n");
+		System.out.println("Pomiar czasu: " + time + " sekund.");
 	}
 
 }
